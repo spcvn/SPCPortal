a27dac390679fa3d0b6442537200a5e6ecb1a67d
Son Nguyen <shigotoviet@gmail.com>
2017-03-21 15:34:38 +0700

    no message

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 class ActivityController extends Controller
 {
     /**
-     * @var EloquentActivity1
+     * @var EloquentActivity1cd
      */
 
     private $activities;
