e7961642eb7c93f035632bd79c27a117e457108f
SangHuynh <sang.huynh@spc-vn.com>
2017-03-21 14:23:22 +0700

    [Update] update style for PT

--- resources/views/ace/element/head.blade.php
+++ resources/views/ace/element/head.blade.php
@@ -7,13 +7,16 @@
 <meta name="description" content="overview &amp; stats" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
+{{ HTML::style('spcvn/ace/css/ace-rtl.min.css') }}
+{{ HTML::style('spcvn/ace/css/select2.min.css') }}
 {{ HTML::style('spcvn/ace/css/style.custom.css') }}
 {{ HTML::style('spcvn/ace/css/bootstrap.min.css') }}
-{{ HTML::style('spcvn/ace/font-awesome/4.5.0/css/font-awesome.min.css') }}
-{{ HTML::style('spcvn/ace/css/fonts.googleapis.com.css') }}
 {{ HTML::style('spcvn/ace/css/ace-skins.min.css') }}
-{{ HTML::style('spcvn/ace/css/ace-rtl.min.css') }}
+{{ HTML::style('spcvn/ace/css/fonts.googleapis.com.css') }}
+{{ HTML::style('spcvn/ace/css/bootstrap-duallistbox.min.css') }}
+{{ HTML::style('spcvn/ace/css/bootstrap-multiselect.min.css') }}
 {{ HTML::style('spcvn/ace/css/ace.min.css', array('class' => 'ace-main-stylesheet', 'id' => 'main-ace-style')) }}
+{{ HTML::style('spcvn/ace/font-awesome/4.5.0/css/font-awesome.min.css') }}
 
 {{ HTML::script('spcvn/ace/js/ace-extra.min.js') }}
 {{ HTML::script('spcvn/ace/js/jquery-2.1.4.min.js') }}
